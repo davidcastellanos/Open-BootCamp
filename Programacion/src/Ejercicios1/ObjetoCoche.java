01ec708f6008c42709129084f222b98a0a289158
davidcastellanos <castellanos.johann@gmail.com>
2022-07-15 19:15:51 -0500

    Added new files

--- Programacion/src/Ejercicios1/ObjetoCoche.java
+++ Programacion/src/Ejercicios1/ObjetoCoche.java
@@ -3,7 +3,7 @@ package Ejercicios1;
 public class ObjetoCoche {
     public static void main(String[] args) {
         Coche miCoche = new Coche();
-        System.out.println("Nuevo número de puestas: " + miCoche.incrementarPuertas());
+        System.out.println("Nuevo número de puertas: " + miCoche.incrementarPuertas());
 
     }
 }
